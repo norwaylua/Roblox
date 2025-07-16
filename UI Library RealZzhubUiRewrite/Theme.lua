ac7aab2970b51ccfa3da007c013f3fc31f12c5e7
norwaylua <hilosarry77@gmail.com>
2025-07-16 22:00:09 +0700

    Update Theme.lua

--- UI Library RealZzhubUiRewrite/Theme.lua
+++ UI Library RealZzhubUiRewrite/Theme.lua
@@ -1,17 +1,17 @@
 return {
     {
-        Name = "Default",
-        Background = "rbxassetid://7877641241",
-        Logo = "rbxassetid://97859621078390",
-        Color1 = Color3.fromRGB(38, 229, 255),
-        Color2 = Color3.fromRGB(20, 255, 20)
-    },
-    {
         Name = "Valentines Day",
         Background = "rbxassetid://12471127452",
         Logo = "rbxassetid://97859621078390",
         Color1 = Color3.fromRGB(233, 149, 211),
         Color2 = Color3.fromRGB(171, 96, 251)
+    },
+    {
+        Name = "Default",
+        Background = "rbxassetid://7877641241",
+        Logo = "rbxassetid://97859621078390",
+        Color1 = Color3.fromRGB(38, 229, 255),
+        Color2 = Color3.fromRGB(20, 255, 20)
     },
     {
         Name = "Orange Red",

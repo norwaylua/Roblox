0587e7b2bc72b66238cf837020b3827228bce719
norwaylua <hilosarry77@gmail.com>
2025-08-31 20:49:54 +0700

    Update and rename A test to Delta support.lua

--- Delta support.lua
+++ Delta support.lua
@@ -1,10 +1,9 @@
---[[
+
 getgenv().AutoFarmV2 = true
 getgenv().FireTools = true
 getgenv().KillAura = true
 getgenv().Range = 1000000 -- dont edit this code frfr
 getgenv().AutoSpinGift = false -- most anyoning 
-]]--
 
 if game.PlaceId == 79704652105017 then
     local StarterGui = game:GetService("StarterGui")

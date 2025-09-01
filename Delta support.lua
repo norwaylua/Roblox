157e6d8b9262f4b6877dcbff9afd05f69f1643e2
norwaylua <hilosarry77@gmail.com>
2025-09-01 15:25:27 +0700

    Update Delta support.lua

--- Delta support.lua
+++ Delta support.lua
@@ -264,6 +264,27 @@ end
         end
     end)
 
+    task.spawn(function()
+    while task.wait() do
+        if not getgenv().FireTools then break end
+
+        local char = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+        local hum = char:FindFirstChildOfClass("Humanoid")
+
+        if hum then
+            local tool = LocalPlayer.Backpack:FindFirstChildWhichIsA("Tool")
+            if tool and not char:FindFirstChildWhichIsA("Tool") then
+                hum:EquipTool(tool)
+            end
+
+            local equipped = char:FindFirstChildWhichIsA("Tool")
+            if equipped then
+                equipped:Activate()
+            end
+        end
+    end
+end)
+
     
     task.spawn(function()
         while task.wait(2) do
